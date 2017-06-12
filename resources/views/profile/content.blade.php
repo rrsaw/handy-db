a0f2ec7f0743c5210d0c1e2bc99bc3104960ed5d
Fede <federicocattaneo1@ied.edu>
2017-06-12 13:53:38 +0200

    Piccoli problemi di culo
    accortezza grafiche_
    _bottoni
    _reviews

--- resources/views/profile/content.blade.php
+++ resources/views/profile/content.blade.php
@@ -29,7 +29,7 @@
                       <i class="fa fa-pencil edit-item" data-attr="{{$item->id}}" data-image="{{$item->images['0']->id}}"></i>
                       {{ Form::open(array('url' => 'items/'.$item->id, 'class' => 'delete-item pull-right')) }}
                           {{ Form::hidden('_method', 'DELETE') }}
-                          <button type="submit">
+                          <button type="submit times_button" class="times_button">
                             <i class="fa fa-times no-padding-right"></i>
                           </button>
                       {{ Form::close() }}
@@ -107,21 +107,38 @@
     @if(count($reviews))
 
         @foreach($reviews as $review)
-          <div class="reviewsProfile">
-            <div class="profileImage">
-              <img style="width:100%" src="{{ asset('images/personal-images/'.$review->reviewer->profileImage->name) }}" alt="{{$review->reviewer->profileImage->name}}">
+          <div class="reviewsProfile row">
+            <div class="profileImage col-lg-2">
+              <img id="profile_image_review" src="{{ asset('images/personal-images/'.$review->reviewer->profileImage->name) }}" alt="{{$review->reviewer->profileImage->name}}">
             </div>
-            <p class="commenter">{{$review->reviewer->name}} {{$review->reviewer->surname}}</p>
-            <p class="comment">{{$review->description}}</p>
-            <section class="rating">
-              <input type="radio" id="star5" name="rating{{$review->id}}" value="5" @if ($review->value == "5") checked @endif/><label class = "full" for="star5" title="Awesome - 5 stars"></label>
-              <input type="radio" id="star4" name="rating{{$review->id}}" value="4" @if ($review->value == "4") checked @endif/><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
-              <input type="radio" id="star3" name="rating{{$review->id}}" value="3" @if ($review->value == "3") checked @endif/><label class = "full" for="star3" title="Meh - 3 stars"></label>
-              <input type="radio" id="star2" name="rating{{$review->id}}" value="2" @if ($review->value == "2") checked @endif/><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
-              <input type="radio" id="star1" name="rating{{$review->id}}" value="1" @if ($review->value == "1") checked @endif/><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
-            </section>
-
-            <span class="date sub-text">{{ date('d M Y', strtotime($review->date)) }}</span>
+            <div class="col-lg-10">
+              <div class="row">
+                <div class="col-lg-10 profile_review">
+                  <p class="commenter">{{$review->reviewer->name}} {{$review->reviewer->surname}}</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-10">
+                  <section class="rating">
+                    <input type="radio" id="star5" name="rating{{$review->id}}" value="5" @if ($review->value == "5") checked @endif/><label class = "full" for="star5" title="Awesome - 5 stars"></label>
+                    <input type="radio" id="star4" name="rating{{$review->id}}" value="4" @if ($review->value == "4") checked @endif/><label class = "full" for="star4" title="Pretty good - 4 stars"></label>
+                    <input type="radio" id="star3" name="rating{{$review->id}}" value="3" @if ($review->value == "3") checked @endif/><label class = "full" for="star3" title="Meh - 3 stars"></label>
+                    <input type="radio" id="star2" name="rating{{$review->id}}" value="2" @if ($review->value == "2") checked @endif/><label class = "full" for="star2" title="Kinda bad - 2 stars"></label>
+                    <input type="radio" id="star1" name="rating{{$review->id}}" value="1" @if ($review->value == "1") checked @endif/><label class = "full" for="star1" title="Sucks big time - 1 star"></label>
+                  </section>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-lg-10">
+                  <p class="comment">{{$review->description}}</p>
+                </div>
+                </div>
+                <div class="row">
+                  <div class="col-lg-4">
+                    <span class="date sub-text">{{ date('d M Y', strtotime($review->date)) }}</span>
+                  </div>
+              </div>
+            </div>
           </div>
       @endforeach
 
